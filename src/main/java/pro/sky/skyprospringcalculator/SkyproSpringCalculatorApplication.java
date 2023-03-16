75060da5b9af0610cba0a1c6981d2338cc5ff55e
Denis_vrn <denisfedorov50@gmail.com>
2023-03-16 23:45:14 +0300

    My first step in spring boot and maven

--- src/main/java/pro/sky/skyprospringcalculator/SkyproSpringCalculatorApplication.java
+++ src/main/java/pro/sky/skyprospringcalculator/SkyproSpringCalculatorApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkyproSpringCalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkyproSpringCalculatorApplication.class, args);
+		SpringApplication.run (SkyproSpringCalculatorApplication.class, args);
 	}
 
 }
